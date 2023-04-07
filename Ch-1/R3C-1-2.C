f882fc0f3cd7db391c8c1f7805ec8326d33997d9
TanishqGhodadra <tanishqghodadra@gmail.com>
2023-04-07 15:55:35 +0530

    Ch: 1 Introduction to C Language

---
+++ Ch-1/R3C-1-2.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+
+	printf("Name\t\t: Nayan\n");
+	printf("Age\t\t: 18\n");
+	printf("Course\t\t: C Language\n");
+	printf("Contact no.\t: 7845129865\n");
+	printf("Hobby\t\t: \"Cricket\"\n");
+	printf("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
+
+	getch();
+}
+/*
+	Escape Sequence Characters :
+		- \n	=> new line
+		- \t	=> tab space from first character
+				=> old	=> 8
+				=> new	=> 4
+		- \"	=> double quote
+		- \\	=> back slash
+*/
